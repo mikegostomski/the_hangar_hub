7179c1fc508d1c7c5c1b51220b77e0eb6b5d8021
Mike Gostomski <mjg@pdx.edu>
2025-11-12 12:30:22 -0500

    Refactored airport requirement code

--- the_hangar_hub/decorators.py
+++ the_hangar_hub/decorators.py
@@ -1,23 +1,17 @@
-from base.services import message_service, utility_service
+from base.services import message_service
 from base.classes.util.env_helper import Log, EnvHelper
 from base.classes.auth.session import Auth
 from functools import wraps
-from urllib.parse import urlparse
-from django.contrib.auth import REDIRECT_FIELD_NAME
-from django.shortcuts import resolve_url
-from base.services import auth_service
 from base.decorators import decorator_sso_redirect, decorator_redirect
 from the_hangar_hub.services import airport_service, tenant_s
 from the_hangar_hub.models.airport import Airport
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseForbidden
-from the_hangar_hub.models.application import HangarApplication
 
 log = Log()
 env = EnvHelper()
 
 
-def require_airport(after_selection_url=None):
+def require_airport():
     """
     If an airport identifier has been saved in the session, get the Airport object and add it to the request
     If an airport has not been selected, have the user select one and save its identifier in the session
@@ -25,61 +19,12 @@ def require_airport(after_selection_url=None):
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
-            invalid_path_id = False
-
-            # Clear any previous post-airport-selection URL
-            env.set_session_variable("thh-after-ap-selection-url", None)
-
-            # Middleware would have already processed any obvious airport
+            # Middleware has already processed airport parameters
             if hasattr(request, "airport") and type(request.airport) is Airport:
                 return view_func(request, *args, **kwargs)
 
-            # Since airport was not found, check for an application (which contains an airport)
-            application_kwarg = kwargs.get("application_id")
-            if application_kwarg:
-                ha = HangarApplication.get(application_kwarg)
-                if ha:
-                    request.airport = ha.airport
-                    request.airport.activate_timezone()
-                    airport_service.save_airport_selection(ha.airport.identifier)
-                    return view_func(request, *args, **kwargs)
-
-            # Since airport was not found, make sure there is no airport_identifier in the URL
-            # If there is, it must be an invalid airport
-            invalid_path_id = kwargs.get("airport_identifier")
-
-            # If identifier was not found or was invalid
-            # Look for records of this user at an airport
-            if request.user.is_authenticated:
-                mgr = airport_service.managed_airport_identifiers()
-                if len(mgr) == 1:
-                    selected_airport = mgr[0]
-                    request.airport = Airport.get(selected_airport)
-                else:
-                    rental_agreements = tenant_s.get_rental_agreements(Auth.current_user())
-                    aids = list(set(
-                        [x.airport.identifier for x in rental_agreements or []]
-                    ))
-                    if len(aids) == 1:
-                        selected_airport = aids[0]
-                        request.airport = Airport.get(selected_airport)
-
-                # If an airport was selected via this method, redirect to the after_selection_url
-                if hasattr(request, "airport") and request.airport:
-                    # The after_selection_url will default to the current url if not provided
-                    airport_service.save_airport_selection(request.airport)
-
-                    send_to = after_selection_url or request.path
-                    if invalid_path_id:
-                        send_to = send_to.replace(invalid_path_id, request.airport.identifier)
-
-                    return decorator_redirect(request, send_to)
-
-            # Identifier was still not found. Send to airport selection page
-            send_to = after_selection_url or request.path
-            if invalid_path_id and invalid_path_id in send_to:
-                send_to = None
-            env.set_session_variable("thh-after-ap-selection-url", send_to)
+            # Airport was not found. Send to airport selection page, then back to this page
+            env.set_session_variable("thh-after-ap-selection-url", request.path)
             return decorator_redirect(request, "public:search")
 
         return _wrapped_view
@@ -95,6 +40,13 @@ def require_airport_manager(redirect_url='/'):
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
+            # Middleware has already processed airport parameters
+            if hasattr(request, "airport") and type(request.airport) is Airport:
+                pass
+            else:
+                # Airport was not found. Send to airport selection page, then back to this page
+                env.set_session_variable("thh-after-ap-selection-url", request.path)
+                return decorator_redirect(request, "public:search")
 
             # If not logged in, redirect to login
             if not request.user.is_authenticated:
@@ -122,7 +74,6 @@ def require_airport_tenant():
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
-
             # If not logged in, redirect to login
             if not request.user.is_authenticated:
                 return decorator_sso_redirect(request)
@@ -135,9 +86,8 @@ def require_airport_tenant():
                     {"rentals": rentals}
                 )
 
-
-            # If airport identifier is in the path, must be tenant at that airport
-            if hasattr(request, "airport") and request.airport.identifier in request.path:
+            # If airport identifier is in the path, must be a tenant at that airport
+            if hasattr(request, "airport") and type(request.airport) is Airport:
                 this_ap_rentals = []
                 for rental in rentals:
                     if rental.hangar.building.airport.identifier == request.airport.identifier:
--- the_hangar_hub/middleware/airport_middleware.py
+++ the_hangar_hub/middleware/airport_middleware.py
@@ -2,8 +2,9 @@ from django.shortcuts import redirect
 from base.services import auth_service
 from django.urls import reverse
 from base.classes.util.app_data import Log, EnvHelper
-from the_hangar_hub.services import airport_service
+from the_hangar_hub.services import airport_service, tenant_s
 from the_hangar_hub.models.airport import Airport
+from the_hangar_hub.models.application import HangarApplication
 
 log = Log()
 env = EnvHelper()
@@ -30,7 +31,40 @@ class AirportMiddleware:
 
             # If invalid airport identifier was included in the URL
             if selected_airport in request.path and not request.airport:
-                selected_airport = None
+                log.warning(f"Airport included in URL was not found ({selected_airport})")
+
+                # Look for records of this user at an airport
+                if request.user.is_authenticated:
+                    # Is user an airport manager?
+                    mgr = airport_service.managed_airport_identifiers()
+                    if len(mgr) == 1:
+                        request.airport = Airport.get(mgr[0])
+                    else:
+                        # Is user an airport tenant?
+                        rental_agreements = tenant_s.get_rental_agreements(request.user)
+                        aids = list(set(
+                            [x.airport.identifier for x in rental_agreements or []]
+                        ))
+                        if len(aids) == 1:
+                            request.airport = Airport.get(aids[0])
+
+                    # If airport was found, redirect to URL with appropriate airport identifier
+                    if request.airport:
+                        old_path = request.path
+                        new_path = request.path.replace(selected_airport, request.airport.identifier)
+                        if old_path != new_path:
+                            return redirect(new_path)
+
+        # If no airport found, look for an application
+        get_parameter = request.GET.get("application_id")
+        post_parameter = request.POST.get("application_id")
+        kwarg = view_kwargs.get("application_id")
+        application_id = get_parameter or post_parameter or kwarg
+        if application_id:
+            ha = HangarApplication.get(application_id)
+            if ha:
+                request.airport = ha.airport
+                airport_service.save_airport_selection(request.airport)
 
         # If found, activate the timezone
         if hasattr(request, "airport") and request.airport:
